444bb4aae99706ce229763125652e89f5b05dcc4
devmbilal <info.bilalshakeel@gmail.com>
2024-10-25 15:16:49 +0500

    Create directory structure for part_1

---
+++ Lab Tasks/Muhammad-Bilal-WAF-LAB-28-SEP/part_1.js
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+
+const folderName = 'part_1'; 
+const folderPath = path.join(process.cwd,folderName);
+
+
+try {
+    await fs.mkdir(folderPath, { recursive: true });
+    console.log('Folder created successfully');
+} catch (error) {
+    console.error(error && error.message);
+}
